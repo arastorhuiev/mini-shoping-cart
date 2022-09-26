728ab39dc5e007e769a0b382bd2baa9456b5c33a
Andrii Rastorhuiev <arastorhuiev@gmail.com>
2022-09-26 22:30:14 +0300

    added context, complete base functional shopping cart, added base markup for store item (cart functional)

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,94 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext);
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext);
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
+
+  function increaseCartQuantity(id: number) {
+    
+    // wtf, why does not it work???
+
+    // setCartItems((currentItems) => {
+    //   if (currentItems.find((item) => item.id === id) === null) {
+    //     return [...currentItems, { id, quantity: 1 }];
+    //   }
+    //   return currentItems.map((item) => {
+    //     if (item.id === id) {
+    //       return { ...item, quantity: item.quantity + 1 };
+    //     }
+    //     return item;
+    //   });
+    // });
+
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id) == null) {
+        return [...currentItems, { id, quantity: 1 }];
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function decreaseCartQuantity(id: number) {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id)?.quantity === 1) {
+        return currentItems.filter((item) => item.id !== id);
+      }
+      return currentItems.map((item) => {
+        if (item.id === id) {
+          return { ...item, quantity: item.quantity - 1 };
+        }
+        return item;
+      });
+    });
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems((currentItems) => {
+      return currentItems.filter((item) => item.id !== id);
+    });
+  }
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+      }}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+}
---
+++ src/utilites/formatCurrency.ts
@@ -0,0 +1,7 @@
+const CURRENCT_FORMATTER = new Intl.NumberFormat(undefined, {
+  currency: 'USD', style: 'currency'
+})
+
+export function formatCurrency(number: number) {
+  return CURRENCT_FORMATTER.format(number)
+}
